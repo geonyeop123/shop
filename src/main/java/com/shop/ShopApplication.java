a14ca7470fc1cfff2bbcb67201248905a2ce1526
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-05-23 21:49:51 +0900

    Querydsl test

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,7 +2,10 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShopApplication {
 
@@ -10,4 +13,8 @@ public class ShopApplication {
         SpringApplication.run(ShopApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
 }
