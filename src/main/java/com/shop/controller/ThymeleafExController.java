f5cee5c35a5a16fbb233e9ac94ce1e0b5915dea8
YEOP <64470512+geonyeop123@users.noreply.github.com>
2022-05-26 21:56:41 +0900

    thymeleaf test

---
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -0,0 +1,68 @@
+package com.shop.controller;
+
+import com.shop.dto.ItemDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafExController {
+
+    @GetMapping("/ex01")
+    public String thymeleafExample01(Model model){
+        model.addAttribute("data", "타임리프 예제입니다.");
+        return "thymeleafEx/thymeleafEx1.html";
+    }
+
+    @GetMapping("/ex02")
+    public String thymeleafExample02(Model model){
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("상품 상세 설명");
+        itemDto.setItemNm("테스트 상품1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02.html";
+    }
+
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 0; i <= 10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(10000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
+    }
+    @GetMapping("/ex04")
+    public String thymeleafExample04(Model model){
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i = 0; i <= 10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx04";
+    }
+}
